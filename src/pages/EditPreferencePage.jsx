acaea482950b69171eb14a469187f9232f5e2871
DooHwanKim0419 <dkimh0419@gmail.com>
2023-10-31 15:41:30 -0500

    Finished EditPreferencePage ui work

--- src/pages/EditPreferencePage.jsx
+++ src/pages/EditPreferencePage.jsx
@@ -1,5 +1,82 @@
+import { Box, useTheme, Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import ProfileHeader from "../components/ProfileHeader";
+import MultiSelect from "../components/MultiSelect";
+import SingleSelect from "../components/SingleSelect";
+import Container from "../components/Container";
+
 const EditPreferencePage = () => {
-  return <h1>asdasdsa</h1>;
+  const theme = useTheme();
+  const navigate = useNavigate();
+
+  return (
+    <Box sx={{ backgroundColor: theme.palette.primary[1], height: "100%" }}>
+      <ProfileHeader />
+      <Container>
+        <MultiSelect
+          label={"Gender"}
+          dbUpdate={null}
+          dbKey={["PartnerPreferences", "Gender"]}
+          showNoPreference={true}
+          options={["Male", "Female", "Nonbinary", "Other"]}
+        />
+        <SingleSelect
+          label={"Age"}
+          options={["18-20", "20-30", "30-40", "40+"]}
+          values={["18-20", "20-30", "30-40", "40+"]}
+          dbUpdate={null}
+          dbKey={["PartnerPreferences", "Age"]}
+        />
+        <SingleSelect
+          label={"Experience Level"}
+          options={[
+            "Beginner (<1 year)",
+            "Intermediate (1-3 years)",
+            "Advanced (3-5 years)",
+            "Expert (5+ years)",
+          ]}
+          values={["beginner", "intermediate", "advanced", "expert"]}
+          dbUpdate={null}
+          dbKey={["PartnerPreferences", "ExperienceLevel"]}
+        />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Button
+            variant="contained"
+            sx={{
+              borderRadius: "40px",
+              marginRight: "30px",
+              backgroundColor: theme.palette.primary["main"],
+              color: theme.palette.text.primary,
+              "&:hover": {
+                backgroundColor: theme.palette.primary["main"],
+              },
+            }}
+          >
+            Save
+          </Button>
+          <Button
+            variant="contained"
+            sx={{
+              borderRadius: "40px",
+              backgroundColor: theme.palette.primary["warning"],
+              color: theme.palette.text.primary,
+              "&:hover": {
+                backgroundColor: theme.palette.primary["warning"],
+              },
+            }}
+          >
+            Discard
+          </Button>
+        </Box>
+      </Container>
+    </Box>
+  );
 };
 
 export default EditPreferencePage;
